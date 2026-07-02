5593241b259658067147419521479cb7d841e7dd
HC1953 <henrik.christiansen.53@gmail.com>
2026-07-02 22:14:30 +0200

    Add fullscreen and GPS button functionality

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// Version 0.1
+document
+.getElementById("fullscreenButton")
+.addEventListener("click",()=>{
+
+if(!document.fullscreenElement){
+
+document.documentElement.requestFullscreen();
+
+}else{
+
+document.exitFullscreen();
+
+}
+
+});
+
+document
+.getElementById("gpsButton")
+.addEventListener("click",()=>{
+
+alert("GPS-funktion kommer i version 2.1");
+
+});
